9f649b8311a4e81633c96f99c088e39775063a59
Valeria Osorio <valeriaosorio@Valerias-MacBook-Pro.local>
2024-03-05 16:47:31 -0500

    Adds static content

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <script setup lang="ts">
+import NavBar from './components/NavBar.vue';
+import HeroCard from './components/HeroCard.vue';
+import BannerTop from './components/BannerTop.vue';
+import BannerBottom from './components/BannerBottom.vue';
+import Footer from './components/FooterComponent.vue';
 </script>
 
 <template>
   Hello world!
+  <NavBar />
+  <HeroCard />
+  <div class="max-centered">
+        <BannerTop />
+        <main role="main">
+            <!-- Start: Implementation -->
+            👉 Your code goes here 👈
+            <!-- End: Implementation -->
+        </main>
+        <BannerBottom />
+        <hr role="separator">
+        <Footer />
+    </div>
 </template>
---
+++ src/components/BannerBottom.vue
@@ -0,0 +1,18 @@
+<template>
+  <aside class="banner banner-bottom" role="doc-tip" aria-label="Submit a name">
+    <img
+      srcset="../assets/img/bg-people.png 750w, ../assets/img/bg-people.@2x.png 1440w"
+      sizes="(min-width: 750px) 1440px, 100vw"
+      class="banner__background"
+      src="../assets/img/bg-people.png"
+      alt=""
+      role="none"
+    />
+    <div class="banner__left">
+      <h2 class="banner__heading">Is there anyone else you would want us to add?</h2>
+    </div>
+    <div class="banner__right">
+      <button class="banner__cta">Submit a name</button>
+    </div>
+  </aside>
+</template>
